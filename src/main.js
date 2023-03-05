2352f27968efcd7922717c8bc05d60108514504c
salem.affa <salem.affa@mehriso.com>
2023-03-05 21:35:42 +0100

    chore: init

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (_, res) => {
+  res.send('Hello world');
+});
+
+app.listen(8900, () => {
+  console.log('Server is up...');
+});
